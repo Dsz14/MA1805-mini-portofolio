aee2f90f84b1a1804b05164abee2c6f040648b9d
Dsz14 <daniel14.genesis@gmail.com>
2025-11-03 13:29:25 +0000

    Add files via upload

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,16 +1,47 @@
-let img; // Declare variable 'img'.
+let img; // A variable to store the image in
+let startx = 0 // starting x coordinate
+let starty = 0 // starting y coordinate
 
 function preload() {
-  img = loadImage('Antenna.jpg'); // Load image
+  img = loadImage('nature.jpeg'); // preloads nature picture!
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // creates a canvas as big as the browser area
+  createCanvas(windowWidth, windowHeight); 
+  // Prepare the loaded image
+  img.loadPixels(); // loads image
+  img.resize(windowWidth, 0); // resizes image to window size
+  img.updatePixels(); // updates image
+  // Border or no border
+  noStroke(); // disables element stroke
 }
 
 function draw() {
-  // Display image at actual size at x/y (0,0)
-  image(img, 0, 0);
-  // Displays half size image at x/y (0, 100)
-  //image(img, 0, 100, img.width/2, img.height/2);
+  clear();
+  //background(0);
+
+  let size = floor(map(mouseX, 0, width, 7, 40)); // maps mouseX value to element size
+  // Loop through all the pixels in the image
+  for (let starty=0; starty<img.height; starty++) { // Rows first
+    for (let startx=0; startx<img.width; startx++) {
+      let index = (startx+starty*img.width)*4;
+
+      // Convert colour image to greyscale
+      let r = img.pixels[index+0];
+      let g = img.pixels[index+1];
+      let b = img.pixels[index+2];
+      let bright = ((0.3*r)+(0.59*g)+(0.11*b)) // calc grayscale
+      fill(bright) // fills element with adjusted grayscale
+
+      // Draw a shape as a pixel 
+      rect(startx, starty, size, size)
+      //triangle(startx, starty, startx + (size / 2), starty + size, startx + size, starty) // upside down triangle
+      //triangle(startx, starty, startx, starty + size, startx + size, starty)
+
+      startx = startx+size -1 // set new startx value
+    }
+    starty = starty+size-1 // set new starty value
+  }
+
 }
